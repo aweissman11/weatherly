4ba5dda92d9a5489dd11a71a41ad316280d2a9c7
Ashton Bailey <ashtonkbailey@gmail.com>
2018-09-05 15:20:24 -0600

    Change file path to state in current weather

--- src/App.js
+++ src/App.js
@@ -9,17 +9,24 @@ import data from './sampleData';
 
 
 class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor() {
+    super();
 
+    this.state = {
+      localStats: data.current_observation,
+      localForecast: data.forecast
+    }
+  }
 
   render() {
     const propsData = {data};
 
     return (
       <div className="App">
-        <CurrentWeather propsData={propsData}/>
+        <CurrentWeather 
+          localStats={this.state.localStats}
+          localForecast={this.state.localForecast}
+        />
         <SevenHour  propsData={propsData}/>
       </div>
     );
--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -3,27 +3,22 @@
 import React, { Component } from 'react';
 import data from './sampleData';
 
-const currentCity = data.current_observation.display_location.full;
-const condition = data.current_observation.weather;
-const currentDay = data.current_observation.observation_time;
-const currentTemp = data.current_observation.temp_f;
-const highTemp = data.forecast.simpleforecast.forecastday[0].high.fahrenheit
-const lowTemp = data.forecast.simpleforecast.forecastday[0].low.fahrenheit
-const weatherDescription = data.forecast.txt_forecast.forecastday[0].fcttext;
-
-
 class CurrentWeather extends Component {
+  constructor() {
+    super();
+  }
+
   render() {
     return (
       <div className='current-weather'>
-        <p>{this.props.currentCity}</p>
-        <p>{this.props.condition}</p>
-        <p>{this.props.currentDay}</p>
-        <p>Current Temperature: {this.props.currentTemp}</p>
-        <p>High: {this.props.highTemp}, Low: {this.props.lowTemp}</p>
-        <p>{this.props.weatherDescription}</p>
+        <p>{this.props.localStats.display_location.full}</p>
+        <p>{this.props.localStats.weather}</p>
+        <p>{this.props.localStats.observation_time}</p>
+        <p>Current Temperature: {this.props.localStats.temp_f}</p>
+        <p>High: {this.props.localForecast.simpleforecast.forecastday[0].high.fahrenheit}, Low: {this.props.localForecast.simpleforecast.forecastday[0].low.fahrenheit}</p>
+        <p>{this.props.localForecast.txt_forecast.forecastday[0].fcttext}</p>
       </div>
-    )
+    );
   }
 }
 
