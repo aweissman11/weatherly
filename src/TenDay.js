52c7b40b05bbb3e9ff102bc4792fb76febc883ec
Aaron Weissman <weissman.aaron@gmail.com>
2018-09-05 16:24:33 -0600

    improve tenday card

--- src/TenDay.js
+++ src/TenDay.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 export default class TenDay extends Component {
   render() {
     return (
-      <h1>Ten Day Forecast</h1>
+      <div className='ten-day-forecast'>
+        <h1>Ten Day Forecast</h1>
+        
+      </div>
       
     )
   }
