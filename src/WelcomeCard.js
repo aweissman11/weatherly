58ade869303d4c1ced04d3ef1514716e11a7d09f
Ashton Bailey <ashtonkbailey@gmail.com>
2018-09-12 20:55:57 -0600

    Adjust spacing, font size, and add background filter

--- src/WelcomeCard.js
+++ src/WelcomeCard.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 import cityList from './largest1000cities';
 
-
 export default class WelcomeCard extends Component {
 	constructor() {
 		super();
@@ -14,7 +13,6 @@ export default class WelcomeCard extends Component {
 		this.changeValue = this.changeValue.bind(this);
 		this.enterValue = this.enterValue.bind(this);
 	}
-
 
 	handleSubmit(event) {
 		event.preventDefault();
@@ -90,7 +88,6 @@ export default class WelcomeCard extends Component {
 							id='recents'
 						>
 							{
-
 								firstTenCities.map( (city, i) => {
 									return (
 										<option value={city} key={i} >{city}</option>
@@ -105,19 +102,3 @@ export default class WelcomeCard extends Component {
 	}
 }
 
-
-// populate suggestions list using complete me
-    //will need to use populate method to fill with thousand cities
-    //then will need to apply autocomplete array (maybe with an added clicked on counter in an obj?)
-    //will then put that in dropdown as user is typing
-// A click on search should:
-    // store searches in local storage
-        // display that as a drop down when search input is clicked
-        //Maybe only last ten searches or something?
-        //Or maybe only last ten are shown in dropdown or recent searches
-            //but rest are saved in an object with a counter on how many times they've been clicked
-            // potentially also keep recency counter (starts at ten, moves down towards one as more things are searched)
-            //then add together total clicks with recency counter for what is displayed at the top
-    //navigate to current weather page
-
-//
